76bd276c42c89d54242e8e13717ce622c42163b7
Jakub Skierbiszewski <jakub.skierbiszewski@norbsoft.com>
2014-09-26 18:27:00 +0200

    ex03

--- src/main/java/pl/touk/wasjawa/java8/exercise/ex03/StockService.java
+++ src/main/java/pl/touk/wasjawa/java8/exercise/ex03/StockService.java
@@ -3,18 +3,13 @@ package pl.touk.wasjawa.java8.exercise.ex03;
 import pl.touk.wasjawa.java8.exercise.common.stocks.Stock;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StockService {
     public String companyNames(List<Stock> stocks) {
-        StringBuilder sb = new StringBuilder("[");
-        for (Stock stock : stocks) {
-            if (sb.length() > 1) {
-                sb.append(", ");
-            }
-            sb.append(stock.getCompanyName());
-        }
-        sb.append("]");
-        return sb.toString();
+	    return stocks.stream()
+			    .map(Stock::getCompanyName)
+	            .collect(Collectors.joining(", ", "[", "]"));
     }
 
 }
