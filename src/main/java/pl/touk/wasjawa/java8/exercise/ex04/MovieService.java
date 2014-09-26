a82566ed9bb311b3d7fc2fa57e9f2488c7f1a0ab
Jakub Skierbiszewski <jakub.skierbiszewski@norbsoft.com>
2014-09-26 18:41:44 +0200

    ex04

--- src/main/java/pl/touk/wasjawa/java8/exercise/ex04/MovieService.java
+++ src/main/java/pl/touk/wasjawa/java8/exercise/ex04/MovieService.java
@@ -6,23 +6,18 @@ import pl.touk.wasjawa.java8.exercise.common.movies.Movie;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MovieService {
 
-    public static final int NEW_MOVIE_YEAR_LIMIT = 2000;
+	public static final int NEW_MOVIE_YEAR_LIMIT = 2000;
 
-    public List<String> polishActorNamesInNewMovies(List<Movie> movies) {
-        List<String> polishActorNames = new ArrayList<>();
-        for (Movie movie : movies) {
-            if (movie.getReleaseYear() > NEW_MOVIE_YEAR_LIMIT) {
-                for (Actor actor : movie.getActors()) {
-                    if (actor.getCountryOfBirth() == CountryCode.PL) {
-                        polishActorNames.add(actor.getName());
-                    }
-                }
-            }
-        }
-        return polishActorNames;
-    }
-
+	public List<String> polishActorNamesInNewMovies(List<Movie> movies) {
+		return movies.stream()
+				.filter(movie -> movie.getReleaseYear() > NEW_MOVIE_YEAR_LIMIT)
+				.flatMap(movie -> movie.getActors().stream())
+				.filter(actor -> actor.getCountryOfBirth() == CountryCode.PL)
+				.map(Actor::getName)
+				.collect(Collectors.toList());
+	}
 }
