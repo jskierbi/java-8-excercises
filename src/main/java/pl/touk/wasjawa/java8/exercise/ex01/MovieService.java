a989b16d55545a54f51937c3488536a2bbb6b8b7
Jakub Skierbiszewski <jakub.skierbiszewski@norbsoft.com>
2014-09-26 17:45:39 +0200

    using streams and collectors

--- src/main/java/pl/touk/wasjawa/java8/exercise/ex01/MovieService.java
+++ src/main/java/pl/touk/wasjawa/java8/exercise/ex01/MovieService.java
@@ -1,22 +1,18 @@
 package pl.touk.wasjawa.java8.exercise.ex01;
 
 import pl.touk.wasjawa.java8.exercise.common.CountryCode;
-import pl.touk.wasjawa.java8.exercise.common.movies.Actor;
 import pl.touk.wasjawa.java8.exercise.common.movies.Movie;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MovieService {
 
-    public List<String> polishActorNames(Movie movie) {
-        List<String> polishActorNames = new ArrayList<>();
-        for (Actor actor : movie.getActors()) {
-            if (actor.getCountryOfBirth() == CountryCode.PL) {
-                polishActorNames.add(actor.getName());
-            }
-        }
-        return polishActorNames;
-    }
+	public List<String> polishActorNames(Movie movie) {
+		return movie.getActors().stream()
+				.filter(actor -> actor.getCountryOfBirth() == CountryCode.PL)
+				.map(actor::getName())
+				.collect(Collectors.toList());
+	}
 
 }
