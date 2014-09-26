13c6e91fe81d831030f2fdb0fe995d8c8e0531e4
Jakub Skierbiszewski <jakub.skierbiszewski@norbsoft.com>
2014-09-26 20:21:14 +0200

    ex10 try (not finished)

--- src/main/java/pl/touk/wasjawa/java8/exercise/ex10/StockService.java
+++ src/main/java/pl/touk/wasjawa/java8/exercise/ex10/StockService.java
@@ -7,6 +7,12 @@ import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 
 public class StockService {
     public BigDecimalStats stocksStats(List<Stock> stocks) {
@@ -28,4 +34,46 @@ public class StockService {
         return new BigDecimalStats(Optional.ofNullable(min), Optional.ofNullable(max), sum, avg, count);
     }
 
+	public static class BigDecimalStatsCollector implements Collector<Stock, Acc, BigDecimalStats> {
+
+		@Override public Supplier<Acc> supplier() {
+			return Acc::new;
+		}
+		@Override public BiConsumer<Acc, Stock> accumulator() {
+			return (acc, stock) -> acc.accumulate(stock);
+		}
+		@Override public BinaryOperator<Acc> combiner() {
+			return (first, second) -> first.add(second);
+		}
+		@Override public Function<Acc, BigDecimalStats> finisher() {
+			return (acc) -> acc.finish();
+		}
+		@Override public Set<Characteristics> characteristics() {
+			return EnumSet.of(Characteristics.UNORDERED);
+		}
+	}
+
+	public static class Acc {
+
+		private final Optional<BigDecimal> min;
+		private final Optional<BigDecimal> max;
+		private final BigDecimal sum;
+		private final BigInteger count;
+
+		public void accumulate(Stock stock) {
+			// TODO min
+			// TODO max
+			sum += stock.getPrice();
+			count += 1;
+		}
+
+		public Acc add(Acc second) {
+			sum += second.sum;
+			count += second.count;
+		}
+
+		public BigDecimalStats finish() {
+			return new BigDecimalStats(Optional.of())
+		}
+	}
 }
